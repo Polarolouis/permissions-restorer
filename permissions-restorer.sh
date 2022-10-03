6c5e782f2b42955d97cef8756caa9b802e5c92dd
Polarolouis <louis.lacoste@hotmail.fr>
2022-10-03 17:06:12 +0200

    Added the header license

--- permissions-restorer.sh
+++ permissions-restorer.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+    A recursive permissions restorer script
+    Copyright (C) 2022 Louis Lacoste
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 usage () {
     echo "Description: $(basename "$0") is an utility script to restore permissions recursively in a folder." 
     echo "To use it you MUST HAVE a backup with correct permissions to act as a reference."
